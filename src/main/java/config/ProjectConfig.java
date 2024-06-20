5f691409ea52e5fdd8ae713190978b30184b15a6
Symphony-1 <aamitaswal02@gmail.com>
2024-06-20 13:28:47 +0530

    test

---
+++ src/main/java/config/ProjectConfig.java
@@ -0,0 +1,24 @@
+package config;
+
+import beans.Cat;
+import beans.Owner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+//    @Bean
+//    public Cat cat (){
+//        Cat c =  new Cat();
+//        c.setName("Tom");
+//        return c;
+//    }
+//    @Bean
+//    public Owner owner(Cat cat){
+//        Owner o =  new Owner();
+//        o.setCat(cat);
+//        return o;
+//    }
+}
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import beans.Cat;
+import beans.Owner;
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        try(var context = new AnnotationConfigApplicationContext(ProjectConfig.class)){
+            Cat x = context.getBean(Cat.class);
+            Owner o = context.getBean(Owner.class);
+            x.setName("Bruno");
+            System.out.println(x);
+            System.out.println(o);
+        }
+    }
+}
